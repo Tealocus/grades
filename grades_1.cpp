2ac444878b087f155b66b5531e8cc03a0146dd4c
Tealocus <telo6727@colorado.edu>
2023-09-17 11:14:43 -0600

    first commit

---
+++ grades_1.cpp
@@ -0,0 +1,24 @@
+// program to print the grade recieved for CSCI 1300
+// > 93 -> A
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    double score = 0;
+
+    cout << "Enter the score you earned for CSCI 1300: ";
+    cin >> score;
+
+    if(score >= 93)
+    {
+        cout << "Grade: A" << endl; 
+    }
+    else if (score >= 90)
+    {
+        cout << "Grade: A-" << endl; 
+    }
+    return 0;
+}
